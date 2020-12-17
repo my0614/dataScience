f1f1f3683caf6690004f10ccf9810b68f774c97f
minyoung <aqi222@naver.com>
2020-12-18 08:54:47 +0900

    연습

---
+++ array.py
@@ -0,0 +1,14 @@
+'''
+numpy를 사용하여 array에 저장하기
+
+'''
+import numpy
+
+arr = numpy.array([1,2,3,4,5,6,7])
+arr2 = numpy.full(1,5)
+arr3 = numpy.random.random(6)
+arr4 = numpy.arange(1,101) 
+arr5 = numpy.arange(1,100,5)
+
+
+print(arr4)
