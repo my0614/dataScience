d0ca3749cb8671e27ef6a98618f2aeda2a6c6bcc
minyoung <aqi222@naver.com>
2021-01-06 12:02:13 +0900

    pie차트로 시각화하기

--- subway.py
+++ subway.py
@@ -5,17 +5,18 @@ f = open('subwayfee.csv')
 data = csv.reader(f)
 next(data)
 
-mx = [0] * 4
-rate = 0
-mx_station = [''] * 4
+
 label = ['유임승차','유임하차','무임승차','무임하차']
+color =['purple', 'yellow', 'pink','green']
+plt.rc('font', family='Malgun Gothic')
 
 
 for row in data:
     for i in range(4,8):
         row[i] = int(row[i])
-        if row[i] > mx[i-4]:
-            mx[i-4] = row[i]
-            mx_station[i-4] = row[3] + '' + row[1]
-for i in range(4):
-    print(label[i]+ ':' + mx_station[i], mx[i])
+        plt.figure(dpi= 300)
+        plt.title(row[3] + '' + row[1])
+        plt.pie(row[4:8], labels= label, colors= color, autopct='%.1f%%')
+        plt.axis('equal')
+        plt.savefig(row[3]+ ' '+row[1]+'.png') # png파일로 저장하기
+        plt.show()
