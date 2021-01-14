7ffe477fc3cccc3ed2e9a77eb6f7579fcd38162e
minyoung <aqi222@naver.com>
2021-01-14 14:29:19 +0900

    한글오류수정 및 x,y축 바꾸기

--- 0114.py
+++ 0114.py
@@ -19,8 +19,12 @@ for row in data:
         result.append(row[5]) #보관대수 결과로
         label.append(row[0]) # 보관수명
 
+result.sort()
+print(result)
 plt.plot(result, label = 'bike count', color = 'purple')
 
+plt.xlabel('보관대수', FontProperties = fontprop)
+plt.ylabel('자전거', FontProperties = fontprop)
 plt.title('%s Bike Storage count' % country, FontProperties = fontprop)
 plt.legend()
 plt.show()
