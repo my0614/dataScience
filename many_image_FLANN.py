4a8114e58a607a87c83a8038873c4f0d024ae9b7
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-01-26 12:00:39 +0900

    Update many_image_FLANN.py
    정확도가 높은 cnt의 값을 딕셔너리로 바꾸어 유사한 이미지 5장 뽑아내기

--- many_image_FLANN.py
+++ many_image_FLANN.py
@@ -4,10 +4,10 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import matplotlib.pyplot as pp
+import time
 
-print("hello")
 input_ = input() # 이미지 파일 입력
-
+start = time.time()
 a = cv2.imread(input_)
 qimg = cv2.resize(a,(300,300))
 
@@ -27,9 +27,12 @@ flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 
 x = 0
+index = 0 # 딕셔너리 key값 변수
 cnt = 1
 count = 0
 result = []
+img_add = {}
+img_dict = {}
 index_max = 0
 # 원하는 디렉토리의 파일의 개수 가져오기
 for pack in os.walk('C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/'):
@@ -37,6 +40,7 @@ for pack in os.walk('C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion
         x +=1
 print('file count = ',x)
 for i in range(1,x+1):
+    index = str(i)
     b = cv2.imread('./image/fasion/fasion_%d.jpg' % cnt)
     cnt +=1
     timg = cv2.resize(b,(300, 300))
@@ -46,21 +50,31 @@ for i in range(1,x+1):
     matchesMask = [[0,0] for i in range(len(matches1))]
 
     for i,(m,n) in enumerate(matches1):
-        if m.distance <0.89*n.distance:
+        if m.distance <0.89*n.distance: #ratio 정확도
             matchesMask[i] = [1.0]
             count += 1
+
+    
+    img_add[index] = count # 딕셔너리로 쉽게 비슷한 이미지 찾기
+    img_dict.update(img_add)
     
     result.append(count) # 정확도 결과리스트에 추가
     count = 0 # 카운트 초기화
     draw_params = dict(matchColor = (255,0,139),singlePointColor = (255,0,139), matchesMask = matchesMask, flags = 0)
     res1 = cv2.drawMatchesKnn(qimg, kp1, timg, kp2, matches1, res1,  **draw_params)
-    plt.imshow(res1)
-    plt.show()
 
-print(result)
-index_max = result.index(max(result))
-print(index_max)
-fileName = r'C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/fasion_%d.jpg' % index_max
-ndarray = img.imread(fileName)
-plt.imshow(ndarray)
-plt.show()
+
+print(sorted(result, reverse = True))
+#가장 비슷한 사진 5장 찾기
+print(time.time() - start)
+
+
+for idx in range(0,5):
+    print(sorted(result, reverse = True)[idx])
+    #fileName = r'C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/fasion_%d.jpg' % sorted(img_dict.items(), key=lambda x: x[1], reverse=True).get()
+    #ndarray = img.imread(fileName)
+    #plt.imshow(ndarray)
+    #plt.show()
+
+
+
