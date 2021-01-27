90a42b7fd6fffe161250c1fd6ad5139566e190a2
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-01-27 10:32:04 +0900

    Update many_image_FLANN.py
    유사한 이미지와 함께 한개의 이미지로 저장 및 make_img,file_cnt 함수 만들기

--- many_image_FLANN.py
+++ many_image_FLANN.py
@@ -6,24 +6,39 @@ import matplotlib.image as img
 import matplotlib.pyplot as pp
 import time
 
+#이미지 만들어주는 함수
+def make_img(img):
+    a = cv2.imread(img)
+    img = cv2.resize(a,(300,300))
+    return img
+
+def file_cnt(path):
+    x = 0
+    for pack in os.walk(path):
+        for f in pack[2]:
+            x +=1
+    return x
+
+
+COLOR = [0,0,0] # 테투리색 정하기
+pixel = 15
 input_ = input() # 이미지 파일 입력
 start = time.time()
-a = cv2.imread(input_)
-qimg = cv2.resize(a,(300,300))
+qimg = make_img(input_)
+constant = cv2.copyMakeBorder(qimg, pixel, pixel, pixel, pixel, cv2.BORDER_CONSTANT, value=COLOR)
+qimg = constant
 res1, res2 = None, None
 
 #sift 연산
 sift = cv2.xfeatures2d.SIFT_create()
-
 kp1,des1 = sift.detectAndCompute(qimg,None)
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks= 50)
-
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 
-x = 0
+
 index = 0 # 딕셔너리 key값 변수
 cnt = 1
 count = 0
@@ -31,17 +46,16 @@ result = []
 img_add = {}
 img_dict = {}
 index_max = 0
+
 # 원하는 디렉토리의 파일의 개수 가져오기
-for pack in os.walk('C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/'):
-    for f in pack[2]:
-        x +=1
-print('file count = ',x)
+
+x = file_cnt('C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/')
 for i in range(1,x+1):
     index = int(i)
-    b = cv2.imread('./image/fasion/fasion_%d.jpg' % cnt)
-    cnt +=1
-    timg = cv2.resize(b,(300, 300))
+    timg = make_img('./image/fasion/fasion_%d.jpg' % cnt)
+    cnt += 1
     kp2,des2 = sift.detectAndCompute(timg,None)
+
     matches1 = flann.knnMatch(des1, des2, k=2) # qimg, timg 를 비교하기
 
     matchesMask = [[0,0] for i in range(len(matches1))]
@@ -50,8 +64,6 @@ for i in range(1,x+1):
         if m.distance <0.90*n.distance: #ratio 정확도
             matchesMask[i] = [1.0]
             count += 1
-
-    
     img_add[index] = count # 딕셔너리로 쉽게 비슷한 이미지 찾기
     img_dict.update(img_add)
     
@@ -59,12 +71,6 @@ for i in range(1,x+1):
     count = 0 # 카운트 초기화
     draw_params = dict(matchColor = (255,0,139),singlePointColor = (255,0,139), matchesMask = matchesMask, flags = 0)
     res1 = cv2.drawMatchesKnn(qimg, kp1, timg, kp2, matches1, res1,  **draw_params)
-    #plt.imshow(res1)
-    #plt.show()
-
-
-print(sorted(result, reverse = True))
-#가장 비슷한 사진 5장 찾기
 
 
 h_list = []
@@ -76,15 +82,12 @@ for idx in range(0,5):
             print(time.time() - start)
             kidx = key
             fileName = r'C:/Users/damons/Desktop/workspace/Datamonsters/image/fasion/fasion_%d.jpg' % int(kidx)
-            ndarray = img.imread(fileName)
-            re_img = cv2.resize(ndarray,(300,300))
-            print("ndarray = {0}".format(re_img.shape))
-            print("qimg = {0}".format(qimg.shape))
-            #print(re_img)
-            h_list.append(re_img)
-            
+            re_img = make_img(fileName)
+            constant = cv2.copyMakeBorder(re_img, pixel, pixel, pixel, pixel, cv2.BORDER_CONSTANT, value=COLOR)
+            h_list.append(constant)
 
-hstack_img = np.hstack((qimg, h_list[0],h_list[1],h_list[2],h_list[3],h_list[4]))
-print("hstack_img.shape = {0}".format(hstack_img.shape))
+
+hstack_img = np.hstack((qimg, h_list[0],h_list[1],h_list[2],h_list[3],h_list[4])) # 유사한 이미지 5개한번에 출력
+cv2.imwrite("copy.jpg", hstack_img);
 plt.imshow(hstack_img)
 plt.show()
