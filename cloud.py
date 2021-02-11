3a2334767fc42df0751e98f4e504043d4dcd8676
minyoung <aqi222@naver.com>
2021-02-11 11:33:50 +0900

    wordclode 모듈설치

---
+++ cloud.py
@@ -0,0 +1,13 @@
+
+import json
+import re
+
+from konlpy.tag import Twitter
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import font_manager, rc
+
+import pytagcloud
+import webbrowser
