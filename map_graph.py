c3fb5f94dd9f7c4add02402cfe7f90f85a1ec768
minyoung <aqi222@naver.com>
2021-02-13 13:08:06 +0900

    지도 그래프 그리기

---
+++ map_graph.py
@@ -0,0 +1,14 @@
+import folium
+
+m = folium.Map(
+    location=[37.5838699,127.0565831],
+    zoom_start=15
+)
+
+folium.Marker(
+  location=[37.5838699,127.0565831],
+  popup='University of Seoul',
+  icon=folium.Icon(color='red',icon='star')
+).add_to(m)
+
+m.save('map.html')
