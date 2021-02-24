1e355822e648d343fcd4152446ea20523ceb24ef
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-02-24 16:54:50 +0900

    Update jeju_covid.py
    확진자와 격리해제 그래프

--- jeju_covid.py
+++ jeju_covid.py
@@ -1,5 +1,4 @@
 #제주도 코로나 현황
-#제주도 코로나 현황
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
@@ -7,11 +6,18 @@ f = open('제주특별자치도코로나현황.csv')
 data = csv.reader(f)
 next(data)
 confirme = []
-date = []
+complete = []
 for row in data:
     confirme.append(row[1])
-    date.append(row[0])
+    complete.append(row[6])
 
-print(date)
-df = pd.DataFrame({'date' : date, 'confirme' : confirme})
-df
+print(confirme)
+print(complete)
+df = pd.DataFrame({'confirme' : confirme})
+df = pd.DataFrame({'complete' : complete})
+
+ax = plt.gca()
+ax.axes.yaxis.set_visible(False)
+
+plt.plot(confirme,'purple')
+plt.plot(complete,'blue')
