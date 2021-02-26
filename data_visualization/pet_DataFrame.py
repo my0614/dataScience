b9eee8a9971dd3f10287b784422715b303319de1
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-02-27 05:32:10 +0900

    Update pet_DataFrame.py
    행정동명에 따라 등록동물수 합계 구하기

--- data_visualization/pet_DataFrame.py
+++ data_visualization/pet_DataFrame.py
@@ -3,14 +3,24 @@ import csv
 f = open('종로구_반려동물등록현황.csv')
 data = csv.reader(f)
 next(data) # header 없애기
+divide = []
 result = []
+table = {}
 for row in data:
     result.append(row)
+    divide.append(row[2])
 df = pd.DataFrame(result)
-df.columns = ['자치구명','법정동명','행정동명','등록동물수','등록품종수','등록소유자수','동물소유자당등록동물수','해당동의등록대행업체수']
+my = set(divide) 
+divide = list(my)
+pd.DataFrame(df)
+for i in divide:
+    df = pd.DataFrame(result)
+    df.columns = ['자치구명','법정동명','행정동명','등록동물수','등록품종수','등록소유자수','동물소유자당등록동물수','해당동의등록대행업체수']
+    pd.DataFrame(df)
+    a= df['행정동명'] == i
+    df = df[a]
+    df = df.astype({'등록동물수':int})
+    to = df['등록동물수'].sum() # 원하는 행 합 구하기
+    table[i] = to
 
-#행정동명 기준으로 분류하기
-pd.DataFrame(df['행정동명'].value_counts())
-
-
-
+table
