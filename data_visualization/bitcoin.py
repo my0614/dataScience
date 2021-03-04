9593cc4c3f96ddd7a94c0dc70b45bf566db65a7c
minyoung <aqi222@naver.com>
2021-03-05 08:38:06 +0900

    비트코인이름정규식으로 한글만 나타내기

--- data_visualization/bitcoin.py
+++ data_visualization/bitcoin.py
@@ -2,10 +2,12 @@ from wordcloud import WordCloud
 from bs4 import BeautifulSoup
 import requests
 import matplotlib.pyplot as plt
+import re
 
 source = requests.get('https://www.bithumb.com/trade/order/BTC_KRW').text
 soup = BeautifulSoup(source, "html.parser")
 hotkeys = soup.select(".tx_l.tx_link") #class가지고 오기
 for name in hotkeys:
-    print(str(name.text).split(' ')[0])
-#trs = a.find_all('td')
+    test = str(name.text).split(' ')[0]
+    test_re = re.compile('[^ \u3131-\u3163\uac00-\ud7a3]+') #정규식(한글만 사용)
+    print(test_re.sub('',test))
