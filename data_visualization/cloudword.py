9b1b31ccb00fa6134c64f3b3b81127192a9d4425
minyoung <aqi222@naver.com>
2021-03-04 22:02:40 +0900

    영화댓글추천순 wordcloud 만들기

--- data_visualization/cloudword.py
+++ data_visualization/cloudword.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 #5페이지만
 movie = []
 for page in range(1,6):
@@ -10,13 +13,18 @@ for page in range(1,6):
     for i in hotkeys:
         movie.append(i.text)
 
-title = {}
+title = ""
 df = pd.DataFrame(movie)
 a = df.value_counts() # 영화댓글순위
 for i in a.items():
-    title[str(i[0]).replace(',','').replace("'",'').replace('(','').replace(')','')] = i[1]
+    title += str(i[0]).replace(',','').replace("'",'').replace('(','').replace(')','').replace(' ','')+" "
 
 print(title)
-
-
-
+#wordcloud만들기
+wordcloud = WordCloud(font_path=r"C:\Users\MY\workspace\datascience\NanumFontSetup_TTF_ALL(1)/NanumGothicCoding.ttf", background_color='white').generate(title)
+#그래프 그리기
+plt.figure(figsize=(22,22)) #이미지 사이즈 지정
+plt.imshow(wordcloud, interpolation='lanczos') #이미지의 부드럽기 정도
+plt.axis('off') #x y 축 숫자 제거
+plt.show() 
+plt.savefig()
