f3503ed588e9d71d5bcde74c06dc21207be20e55
minyoung <aqi222@naver.com>
2021-03-05 11:28:17 +0900

    url링크 다시 크롤링

--- data_visualization/bitcoin.py
+++ data_visualization/bitcoin.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import requests
 import matplotlib.pyplot as plt
 import re
-
-source = requests.get('https://www.bithumb.com/trade/order/BTC_KRW').text
-soup = BeautifulSoup(source, "html.parser")
-hotkeys = soup.select(".tx_l.tx_link") #class가지고 오기
-for name in hotkeys:
-    test = str(name.text).split(' ')[0]
-    test_re = re.compile('[^ \u3131-\u3163\uac00-\ud7a3]+') #정규식(한글만 사용)
-    print(test_re.sub('',test))
+import threading, time
+def crow():
+    source = requests.get('https://www.bithumb.com/').text
+    soup = BeautifulSoup(source, "html.parser")
+    hotkeys = soup.select(".sort_coin_box") #class가지고 오기
+    price = soup.select(".sort_real")
+    print(len(hotkeys))
+    for i in range(1,len(hotkeys)):
+        print(str(hotkeys[i].text),str(price[i].text))
+crow()
