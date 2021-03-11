b1f35da8df0fb04226c63549a0ea2a3aa80aa4b0
minyoung <aqi222@naver.com>
2021-03-11 22:43:40 +0900

    !

---
+++ data_visualization/deeplearning/chatbot/train_tools/qna/create_train_data_table.py
@@ -0,0 +1,26 @@
+import pymysql
+from config.DatabaseConfig import *
+print('hello')
+db = None
+try:
+    print('success')
+    db = pymysql.connect(host= DB_HOST,user=DB_USER, passwd = DB_PASSWORD, db = DB_NAME, charset ='utf8')
+
+    sql = '''
+    CREATE TABLE IF NOT EXISTS `chatbot_train_data` (
+        `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `intent` VARCHAR(45) NULL,
+        `ner` VARCHAR(1024) NULL,
+        `query` TEXT NULL,
+        `answer` TEXT NOT NULL,
+        `answer_image` VARCHAR(2048) NULL,
+        PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET = utf8
+    '''
+    with db.cursor()as cursor:
+        coursor.execute(sql)
+except Exception as e:
+    print(e)
+finally:
+    if db is not None:
+        db.close()
