eb2d486f96f024e2e43020ee0aa4d9256083548a
minyoung <aqi222@naver.com>
2021-03-17 22:57:23 +0900

    restapi구현

---
+++ data_visualization/deeplearning/chatbot/hello_flask/basic_restapi/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+resource = []
+
+@app.route('/user/<int:user_id>', methods= ['GET'])
+#userid 가지고 오기
+def get_user(user_id):
+    for user in resource:
+        if user['user_id'] is user_id:
+            return jsonify(user)
+    return jsonify(None)
+
+@app.route('/user', methods = ['POST'])
+def add_user():
+    user = request.get_json()
+    resource.append(user)
+    return jsonify(resource)
+
+if __name__ == '__main__':
+    app.run()
