16368efacf1ddf0e6a6e80b8ab90b926462e508c
minyoung <aqi222@naver.com>
2021-03-31 17:35:51 +0900

    그래프 추가

--- data_visualization/한국교통안전공단자격증.py
+++ data_visualization/한국교통안전공단자격증.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from matplotlib import rc
 
 data = pd.read_csv('./한국교통안전공단_자격증.csv',encoding='cp949')
+
+fontprop = fm.FontProperties(fname=r"C:\Users\MY\workspace\datascience\NanumFontSetup_TTF_ALL(1)/NanumGothicCoding.ttf").get_name()
+rc('font', family=fontprop)
+
+
 result = []
 label_name = []
 for i in data:
@@ -20,3 +28,9 @@ min_name =df.index[df['Certificate'] == min][0]
 
 print('가장많은자격증을 취득한 것은',max_name,'취득한 사람의 수는 ',max)
 print('가장많은자격증을 취득한 것은',min_name,'취득한 사람의 수는 ',min)
+
+x = np.arange(3)
+plt.figure(figsize=(12, 3))
+plt.bar(label_name,result)
+plt.show()
+
