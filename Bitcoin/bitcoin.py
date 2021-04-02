056f3572782718663c2eb71ca476d2660c1ffe94
minyoung <aqi222@naver.com>
2021-04-02 23:05:06 +0900

    bitcoin웹크롤링

---
+++ Bitcoin/bitcoin.py
@@ -0,0 +1,20 @@
+from wordcloud import WordCloud
+from bs4 import BeautifulSoup
+import requests
+import matplotlib.pyplot as plt
+import re,time
+from threading import Thread
+
+def crow():
+    print('crow')
+    source = requests.get('https://www.bithumb.com/').text
+    soup = BeautifulSoup(source, "html.parser")
+    hotkeys = soup.select(".sort_coin_box") #class가지고 오기
+    price = soup.select(".sort_real")
+    print(len(hotkeys))
+    for i in range(1,len(hotkeys)):
+        a = str(hotkeys[i].text).split(' ')[0]
+        print(str(a),str(price[i].text))
+    time.sleep(1) # 1초 delay
+
+
